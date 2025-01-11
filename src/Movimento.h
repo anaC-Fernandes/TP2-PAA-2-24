55e97501012c007ce5ec11a203b9890551b972f3
Gabriel_Duarte <herreduarteb@gmail.com>
2025-01-11 01:38:12 -0300

    Add do TAD movimento

---
+++ src/Movimento.h
@@ -0,0 +1,9 @@
+#ifndef MOVIMENTO_H
+#define MOVIMENTO_H
+#include "Labirinto.h"
+void movimenta_estudante(const labirinto cave, estudante *cdc, cave_infos infos);
+bool problema_valido(cave_infos infos, estudante cdc);
+
+
+
+#endif //MOVIMENTO_H
