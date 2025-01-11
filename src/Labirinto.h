85bbabf630359d21e8e3eb0bb3eb92bf797117b0
Gabriel_Duarte <herreduarteb@gmail.com>
2025-01-11 00:32:30 -0300

    Add das funções de movimento e da main

--- src/Labirinto.h
+++ src/Labirinto.h
@@ -1,17 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
+
+
 
 typedef struct cell {
     int action;     // ação
+    int peso;       // peso
     char value[4];     // valor
-    bool isstart;   // eComeço
-    bool isend;     // eFim
+    bool isstart;   // Começo
+    bool isEnd;     // Fim
+    bool isStudent;  // Estudante
+    bool wasCalculated; // foi Calculado
 } caverna;
+
+typedef struct estudante {
+    int linha,coluna;
+    int vida;
+    bool isAtEndPoint;
+}estudante;
+
+typedef struct cave_infos {
+    int linhaFim, coluna_fim;
+    int ordem[2];
+}cave_infos;
 
 typedef caverna** labirinto;
 
 labirinto Alocar_Labirinto(int linhas, int colunas);
 void Destrutor_Labirinto(labirinto tabuleiro, int linhas);
 void Imprimir_Labirinto(labirinto tabuleiro, int linhas, int colunas);
-labirinto Processar_Arquivo(FILE* arquivo, int linhas, int colunas);
+labirinto Processar_Arquivo(FILE* arquivo, int linhas, int colunas,  estudante *cdc, cave_infos *infos);
+void get_infos(FILE* arquivo, int* linhas, int* colunas, int* pts_vida);
+void Criar_Estudante(int vida, estudante *cdc);
+void movimenta_estudante(const labirinto cave, estudante *cdc, cave_infos infos);
+bool problema_valido(cave_infos infos, estudante cdc);
--- src/main.c
+++ src/main.c
@@ -1,20 +1,71 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "Labirinto.c"
-
+#include "Labirinto.h"
 
 //main de teste
-int main(){
+/*int main(){
+    estudante cdc;
+    cave_infos infos;
     char buffer[128];
-    printf("Digite o caminho do arquivo: ");
-    scanf("%s", buffer);
+    //printf("Digite o caminho do arquivo: ");
+    //scanf("%s", buffer);
+    strcpy(buffer, "/home/gabrielbd/Codes/TP2-PAA-2-24/lib/teste.txt");
     FILE* arquivo = fopen(buffer,"r");
     int linhas = 0, colunas = 0, pts_vida = 0;
     get_infos(arquivo, &linhas, &colunas, &pts_vida);
+    infos.ordem[0] = linhas - 1;
+    infos.ordem[1] = colunas - 1;
     
-    labirinto tabuleiro = Processar_Arquivo(arquivo,linhas,colunas);
+
+
+    labirinto tabuleiro = Processar_Arquivo(arquivo,linhas,colunas, &cdc, &infos);
+    Criar_Estudante(pts_vida, &cdc);
+    if (problema_valido(infos, cdc)) {
+        movimenta_estudante(tabuleiro, &cdc, infos);
+    }
+
+    
     // Use the 'tabuleiro' variable to avoid unused variable warning
     // For example, you can print or process it
+    
+    printf("------------------//------------------\n");
+    Imprimir_Labirinto(tabuleiro, linhas, colunas);
+    Destrutor_Labirinto(tabuleiro, linhas);
+    fclose(arquivo);
+    return 0;
+}*/
+
+
+// main final
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("%d\n", argc);
+        printf("Digitar somente o caminho e o nome do arquivo de entrada\n");
+        return 1;
+    }
+    printf("Arquivo de entrada: %s\n", argv[1]);
+
+    
+    estudante cdc;
+    cave_infos infos;
+    char buffer[128];
+    
+    strcpy(buffer, argv[1]);
+    FILE* arquivo = fopen(buffer,"r");
+    int linhas = 0, colunas = 0, pts_vida = 0;
+    get_infos(arquivo, &linhas, &colunas, &pts_vida);
+    infos.ordem[0] = linhas - 1;
+    infos.ordem[1] = colunas - 1;
+    
+
+
+    labirinto tabuleiro = Processar_Arquivo(arquivo,linhas,colunas, &cdc, &infos);
+    Criar_Estudante(pts_vida, &cdc);
+    if (problema_valido(infos, cdc)) {
+        movimenta_estudante(tabuleiro, &cdc, infos);
+    }
+
+    
 
     printf("------------------//------------------\n");
     Imprimir_Labirinto(tabuleiro, linhas, colunas);
@@ -22,28 +73,3 @@ int main(){
     fclose(arquivo);
     return 0;
 }
-
-
-// main final
-/*int main(int argc, char *argv[]) {
-    if (argc < 2) {
-        printf("Uso: %s <caminho-do-arquivo-mapa> <caminho-do-arquivo-resultado>\n", argv[0]);
-        return 1;
-    }
-
-
-    FILE* arquivo = fopen(argv[1],"r");
-    int linhas = 0, colunas = 0, pts_vida = 0;
-    get_infos(arquivo, &linhas, &colunas, &pts_vida);
-    
-    labirinto tabuleiro = Processar_Arquivo(arquivo,linhas,colunas);
-    // Use the 'tabuleiro' variable to avoid unused variable warning
-    // For example, you can print or process it
-
-    printf("------------------//------------------\n");
-    Imprimir_Labirinto(tabuleiro, linhas, colunas);
-    Destrutor_Labirinto(tabuleiro, linhas);
-    fclose(arquivo);
-
-    return 0;
-}*/
